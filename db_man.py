b8abc8f637b3444b367d797fbd1ab3a1077dc63f
Mauricio Barba da Costa <barba@mit.edu>
2021-01-20 09:17:39 -0500

    fixed some issues that arose from merging

--- db_man.py
+++ db_man.py
@@ -1,10 +1,10 @@
 import market_data
 
 class stock:
-    from accounts import access_db
     def __init__(self, ticker):
+        from accounts import access_db
         database = access_db()
-        stock_db = database.sheet1
+        stock_db = database.worksheet('Stocks')
         for stock_in_db in stock_db.get_all_records():
             if stock_in_db['Ticker'] == ticker:
                 stock_dict = stock_in_db
@@ -12,27 +12,31 @@ class stock:
         self.ticker = ticker
         self.curr_price = stock_dict['Current Price']
         self.position = stock_dict['Position']
+        self.dict = stock_dict
 
 class user:
-    from accounts import access_db
     def __init__(self, username):
-        database = accounts.access_db()
-        user_db = database.sheet2
+        from accounts import access_db
+        database = access_db()
+        user_db = database.worksheet('Users')
         for user_in_db in user_db.get_all_records():
             if user_in_db['Username'] == username:
                 user_dict = user_in_db
                 break
         self.username = username
         self.accuracy = user_dict['Accuracy']
+
+        self.dict = user_dict
     
-def update_sentiment()
+def update_sentiment():
+    pass
 
 
 def update_stock(date, num_bars): #date in ISO format
     from accounts import access_db
 
     database = access_db()
-    stock_db = database.sheet1
+    stock_db = database.worksheet('Stocks')
 
     for r in range(2, stock_db.row_count -990):
         row = stock_db.row_values(r)
@@ -46,7 +50,7 @@ def update_users(date):
     import public_data 
 
     database = accounts.access_db()
-    users_db = database.sheet2
+    users_db = database.worksheet('Stocks')
 
     for users in users_db.get_all_records():
         username = users['Username']
